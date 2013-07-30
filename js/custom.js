ab709b924db134eeb3c60ffe4a5ff8302da510e9
Peter McAtominey <mcatominey08@gmail.com>
2013-07-30 23:46:27 +0100

    Javascript workaround for mobile nag bar

--- js/custom.js
+++ js/custom.js
@@ -7,4 +7,17 @@ $(function() {
 
 	// Bootstrap Table Class
 	$('table').addClass('table');
+
+	var mobileNavWorkaround = function() {
+		if($(window).width()< 768) {
+			$('#sub-nav-collapse').css('height', '0px');
+		} else {
+			$('#sub-nav-collapse').css('height', 'auto');
+		}
+	}
+
+	$(window).resize(function() {
+		mobileNavWorkaround();
+	});
+	mobileNavWorkaround();
 });
