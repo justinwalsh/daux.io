da7c7ed14c5f2b77da8c4ac37c2f7ca6ecac47b3
Gautham Warrier <gautham.r.w@gmail.com>
2014-08-22 11:43:57 +0530

    Add ability to push doc to bottom of index

--- libs/daux_directory.php
+++ libs/daux_directory.php
@@ -96,13 +96,33 @@
             $name_a = explode('_', $a->name);
             $name_b = explode('_', $b->name);
             if (is_numeric($name_a[0])) {
-                if (is_numeric($name_b[0])) return (intval($name_a[0]) > intval($name_b[0]));
-                return -1;
+                $a = intval($name_a[0]);
+                if (is_numeric($name_b[0])) {
+                    $b = intval($name_b[0]);
+                    if (($a >= 0) == ($b >= 0)) {
+                        $a = abs($a);
+                        $b = abs($b);
+                        if ($a == $b) return (strcasecmp($name_a[1], $name_b[1]));
+                        return ($a > $b) ? 1 : -1;
+                    }
+                    return ($a >= 0) ? -1 : 1;
+                }
+                $t = $name_b[0];
+                if ($t && $t[0] === '-') return -1;
+                return ($a < 0) ? 1 : -1;
             } else {
-                if (is_numeric($name_b[0])) return 1;
-                return (strcasecmp($name_a[0], $name_b[0]));
+                if (is_numeric($name_b[0])) {
+                    $b = intval($name_b[0]);
+                    if ($b >= 0) return 1;
+                    $t = $name_a[0];
+                    if ($t && $t[0] === '-') return 1;
+                    return ($b >= 0) ? 1 : -1;
+                }
+                $p = $name_a[0];
+                $q = $name_b[0];
+                if (($p && $p[0] === '-') == ($q && $q[0] === '-')) return strcasecmp($p, $q);
+                else return ($p[0] === '-') ? 1 : -1;
             }
         }
-
     }
 ?>
