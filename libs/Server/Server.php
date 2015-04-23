f3d472363d01c12b197ee3e770bc0817c1b22231
Stéphane Goetz <onigoetz@onigoetz.ch>
2015-04-23 21:04:00 +0200

    Cleanup

--- libs/Server/Server.php
+++ libs/Server/Server.php
@@ -37,7 +37,7 @@ class Server
     {
         $this->params = $this->daux->getParams();
 
-        $request = Helper::getRequest();
+        $request = $this->getRequest();
         $request = urldecode($request);
         $request_type = isset($query['method']) ? $query['method'] : '';
         if ($request == 'first_page') {
@@ -127,5 +127,39 @@ class Server
             $params['entry_page'] = $file->first_page;
         }
         return MarkdownPage::fromFile($file, $params);
+    }
+
+    public function getRequest()
+    {
+        if (isset($_SERVER['PATH_INFO'])) {
+            $uri = $_SERVER['PATH_INFO'];
+        } elseif (isset($_SERVER['REQUEST_URI'])) {
+            $uri = $_SERVER['REQUEST_URI'];
+            if (strpos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
+                $uri = substr($uri, strlen($_SERVER['SCRIPT_NAME']));
+            } elseif (strpos($uri, dirname($_SERVER['SCRIPT_NAME'])) === 0) {
+                $uri = substr($uri, strlen(dirname($_SERVER['SCRIPT_NAME'])));
+            }
+            if (strncmp($uri, '?/', 2) === 0) {
+                $uri = substr($uri, 2);
+            }
+            $parts = preg_split('#\?#i', $uri, 2);
+            $uri = $parts[0];
+            if (isset($parts[1])) {
+                $_SERVER['QUERY_STRING'] = $parts[1];
+                parse_str($_SERVER['QUERY_STRING'], $_GET);
+            } else {
+                $_SERVER['QUERY_STRING'] = '';
+                $_GET = array();
+            }
+            $uri = parse_url($uri, PHP_URL_PATH);
+        } else {
+            return false;
+        }
+        $uri = str_replace(array('//', '../'), '/', trim($uri, '/'));
+        if ($uri == "") {
+            $uri = "first_page";
+        }
+        return $uri;
     }
 }
