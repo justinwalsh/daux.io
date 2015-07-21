046a419c3116b5236fd8375db43e35963400fcff
Stéphane Goetz <onigoetz@onigoetz.ch>
2015-07-21 22:37:29 +0200

    Passed configuration to gulp, due to grunt limitations

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,6 @@ module.exports = function (grunt) {
     'use strict';
 
     grunt.loadNpmTasks('grunt-php');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
         php: {
@@ -15,32 +13,8 @@ module.exports = function (grunt) {
                     router: "index.php"
                 }
             }
-        },
-        less: {
-            development: {
-                options: {
-                    cleancss: true,
-                    report: 'min'
-                },
-                files: {
-                    "resources/themes/daux-blue/css/theme.min.css": "resources/themes/daux-blue/less/theme.less",
-                    "resources/themes/daux-green/css/theme.min.css": "resources/themes/daux-green/less/theme.less",
-                    "resources/themes/daux-navy/css/theme.min.css": "resources/themes/daux-navy/less/theme.less",
-                    "resources/themes/daux-red/css/theme.min.css": "resources/themes/daux-red/less/theme.less"
-                }
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['templates/default/theme/**/*.less'],
-                tasks: ['less'],
-                options: {
-                    nospawn: true
-                },
-            },
-        },
+        }
     });
 
-    //grunt.registerTask('default', ['less', 'watch']);
     grunt.registerTask('default', ['php']);
 };
