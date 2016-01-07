02ede2220ee2a03f10abd6fbff7a40568fd60630
Stéphane Goetz <onigoetz@onigoetz.ch>
2016-01-07 22:16:21 +0100

    Add missing symfony/polyfill-mbstring to the compiled phar

--- libs/Compiler.php
+++ libs/Compiler.php
@@ -54,6 +54,7 @@ class Compiler
             ->ignoreVCS(true)
             ->exclude('Tests')
             ->in(__DIR__ . '/../vendor/symfony/console')
+            ->in(__DIR__ . '/../vendor/symfony/polyfill-mbstring')
             ->in(__DIR__ . '/../vendor/guzzlehttp/guzzle/src/')
             ->in(__DIR__ . '/../vendor/guzzlehttp/ringphp/src/')
             ->in(__DIR__ . '/../vendor/guzzlehttp/streams/src/')
