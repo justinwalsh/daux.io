8e065982c5d73470b73874f7cc58649a7bdd9d7b
Stéphane Goetz <onigoetz@onigoetz.ch>
2016-01-31 11:59:35 +0100

    Small tweaks

--- libs/Tree/Content.php
+++ libs/Tree/Content.php
@@ -72,6 +72,10 @@ class Content extends Entry
 
     public function isIndex()
     {
+        // At some point, it was recommended that
+        // an index page starts with an underscore.
+        // This is not mandatory anymore, both with
+        // and without underscore are supported.
         return $this->name == 'index' || $this->name == '_index';
     }
 
