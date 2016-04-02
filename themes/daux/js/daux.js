2bd1978f0e032d226a32c8774cae03d370c144d6
Stéphane Goetz <onigoetz@onigoetz.ch>
2016-04-02 13:39:04 +0200

    Add an arrow to open sub-trees, fixes #277

--- themes/daux/js/daux.js
+++ themes/daux/js/daux.js
@@ -98,6 +98,12 @@ $(function () {
         $(this).next().slideToggle();
     });
 
+    // New Tree navigation
+    $("ul.nav.nav-list > li.has-children > a > .arrow").click(function() {
+        $(this).parent().parent().toggleClass('open');
+        return false;
+    });
+
     // Responsive navigation
     $('#menu-spinner-button').click(function () {
         $('#sub-nav-collapse').slideToggle();
